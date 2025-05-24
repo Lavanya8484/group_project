4d11640f5b36d001fe135ffef8c1be7474a3d15e
Lavanya8484 <durgavijay501@gmail.com>
2025-05-24 09:31:07 +0530

    commiting changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import NavBar from "./components/sindhu/NavBar";
+import SideBar from "./components/Lavanya/SideBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <div style={{ display: "flex" }}>
+        <SideBar />
+        <div style={{ marginLeft: "200px", padding: "1rem" }}>
+          <h2>Main Content Area</h2>
+          <p>Welcome to the app!</p>
+        </div>
+      </div>
     </div>
   );
 }
