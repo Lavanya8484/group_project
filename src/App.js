cfd121437a564968d9f7471b59b8b2509a94832d
TuppuduSindhu <tuppudu.sgit config --global user.email 2203031241341@paruluniversity.ac.in>
2025-05-24 09:23:59 +0530

    initial mesg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './components/sindhu/NavBar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <div style={{ display: 'flex' }}>
+    
+        <div style={{ marginLeft: '200px', padding: '1rem' }}>
+          <h2>Main Content Area</h2>
+          <p>Welcome to the app!</p>
+        </div>
+      </div>
     </div>
   );
 }
